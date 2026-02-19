31c13ee6539d11c684de8ab919b23e86b3791868
Mustafa9221 <moustafa.kahi9a@gmail.com>
2026-02-20 00:11:57 +0500

    changed the fronted structure to zip file

--- app/api/v1/api_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import detect_video, health
-
-api_router = APIRouter(prefix="/api/v1")
-
-api_router.include_router(detect_video.router, tags=["Detection"])
-api_router.include_router(health.router, tags=["Health"])
